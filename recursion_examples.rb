78dd1d9af4661a7c500f724e452288adba6e4bd6
joezini <josephlukeedwards@gmail.com>
2016-05-14 10:18:12 +0100

    Complete code quiz on recursion

--- recursion_examples.rb
+++ recursion_examples.rb
@@ -45,9 +45,9 @@ puts fib(20)
 
 def flatten(arr)
 	result = []
-	arr.each do |el|
-		if el.is_a?(Array) 
-			flatten(el)
+	arr.each_with_index do |el, i|
+		if el.is_a?(Array)
+			result += flatten(el)
 		else
 			result << el
 		end
@@ -55,6 +55,85 @@ def flatten(arr)
 	result
 end
 
+print flatten([1,2,[3,4],5,6])
 print flatten([1, 2, [3, [4, 5]]])
 print flatten([1, 2, 3])
+print flatten([1, [2], 3])
 
+def int_to_roman(int)
+	decimal_to_roman_mapping = {
+	  1000 => "M",
+	  900 => "CM",
+	  500 => "D",
+	  400 => "CD",
+	  100 => "C",
+	  90 => "XC",
+	  50 => "L",
+	  40 => "XL",
+	  10 => "X",
+	  9 => "IX",
+	  5 => "V",
+	  4 => "IV",
+	  1 => "I"
+	}
+
+	if int > 0
+		this_char = ""
+		rem = 0
+		decimal_to_roman_mapping.each do |k, v|
+			if int >= k
+				this_char = v
+				rem = int - k
+				break
+			end
+		end
+		this_char << int_to_roman(rem)
+	else
+		""
+	end
+end
+
+puts int_to_roman(2)
+puts int_to_roman(7)
+puts int_to_roman(55)
+puts int_to_roman(643)
+
+def roman_to_int(rom)
+	roman_mapping = {
+	  "M" => 1000,
+	  "CM" => 900,
+	  "D" => 500,
+	  "CD" => 400,
+	  "C" => 100,
+	  "XC" => 90,
+	  "L" => 50,
+	  "XL" => 40,
+	  "X" => 10,
+	  "IX" => 9,
+	  "V" => 5,
+	  "IV" => 4,
+	  "I" => 1
+	}
+
+	result = 0
+	rem = ""
+
+	if rom.length > 0
+		roman_mapping.each do |k, v|
+			if rom.index(k) == 0
+				result += v
+				rem = rom.sub(k, "")
+				break
+			end
+		end
+		result += roman_to_int(rem)
+	else
+		0
+	end
+	result
+end
+
+puts roman_to_int("III")
+puts roman_to_int("IX")
+puts roman_to_int("XXXXVI")
+puts roman_to_int("MDLIX")
